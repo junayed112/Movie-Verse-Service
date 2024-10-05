c8514cbb9c901096e1936ae82ef7e2543c7c8a2b
Md. Junayed Hossain <54529889+junayed112@users.noreply.github.com>
2024-10-05 10:55:46 +0600

    Add files via upload
    Initializing the project with required dependencies

---
+++ movie_verse_service/src/main/java/com/example/movie_verse_service/MovieVerseServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.movie_verse_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieVerseServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieVerseServiceApplication.class, args);
+	}
+
+}
---
+++ movie_verse_service/src/test/java/com/example/movie_verse_service/MovieVerseServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.movie_verse_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieVerseServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
